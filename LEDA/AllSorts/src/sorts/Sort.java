2cc0f10b68610a824e0de84180e7e724fbc94042
Jose Ramon Fragoso da Silva 116210412 <joserfs@lcc2-12.lcc.ufcg.edu.br>
2017-11-13 11:02:17 -0300

    sla

--- LEDA/AllSorts/src/sorts/Sort.java
+++ LEDA/AllSorts/src/sorts/Sort.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Sort {
 	
-	public static int particionamento(int[] array, int ini, int fim) {
+	public static int particionamentoLomuto(int[] array, int ini, int fim) {
 		
 		// cria variaveis
 		// j elemento a ser trocado antes do pivot
@@ -32,10 +32,41 @@ public class Sort {
 		return j;
 	}
 	
+	public static int particionamentoHoare(int[] array, int ini, int fim) {
+		
+		// inicializa dois ponteiros onde esq eh o inicio e dir o fim do array
+		int pivot = array[ini];
+		int esq = ini - 1;
+		int dir = fim + 1;
+		
+		while(true) {
+		
+			// aumenta esq ate que array[esq] seja maior ou igual ao pivot
+			do {
+				esq++;
+			}while(array[esq] < pivot);
+
+			// diminui dir ate que array[dir] seja menor ou igual ao pivot
+			do {
+				dir--;
+			}while(array[dir] > pivot);
+			
+			// se esq for menor que dir troca a posicao de array
+			if(esq < dir) {
+				int aux = array[esq];
+				array[esq] = array[dir];
+				array[dir] = aux;
+			} else {
+				// se nao retorna a posicao de dir que sera a posicao do novo pivot
+				return dir;
+			}
+		}
+	}
+	
 	public static void QuickSorting(int[] array, int ini, int fim) {
 		
 		if(ini < fim) {
-			int pos_pivot = particionamento(array, ini, fim);
+			int pos_pivot = particionamentoHoare(array, ini, fim);
 			QuickSorting(array, ini, pos_pivot);
 			QuickSorting(array, pos_pivot+1, fim);
 		}
@@ -126,10 +157,78 @@ public class Sort {
 		System.out.println(Arrays.toString(b));
 		return b;
 	}
+	
+	public static void bubbleSort(int[] array, int ini, int fim) {
+		
+		int aux;
+		for (int i = ini; i <= fim; i++) {
+			for (int j = ini; j <= fim-1; j++) {
+				if(array[j] > array[j+1]) {
+					aux = array[j];
+					array[j] = array[j+1];
+					array[j+1] = aux;
+				}
+			}
+		}
+		
+	}
+	
+	public static void selectionSort(int[] array, int ini, int fim) {
+		
+		for (int i = ini; i <= fim; i++) {
+			int menor = i;
+			for (int j = i + 1; j <= fim; j++) {
+				if (array[menor] > array[j]) {
+					menor = j;
+				}
+			}
+			int aux = array[i];
+			array[i] = array[menor];
+			array[menor] = aux;	
+		}
+	}
 
+	public static void insertionSort(int[] array, int ini, int fim) {
+		
+		int aux, j;
+		
+		for (int i = ini + 1; i <= fim; i++) {
+
+			aux = array[i];
+			j = i;
+			
+			while(j > ini && array[j-1] > aux) {
+				array[j] = array[j-1];
+				j--;
+			}
+			array[j] = aux;		
+		}
+		
+		
+	}
 	public static void main(String[] args) {
 		
-		int[] a = {6,6,6,-1,6,6};		
-		countingSorting(a, 0, a.length-1);
+		int[] array1 = {6,6,6,6,6,6};
+		int[] array2 = {-1,-5,-2,-5,-4,-3,-2};		
+		int[] array3 = {5,-1,-1,2,3};		
+		int[] array4 = {6,5,4,3,2,1};		
+		int[] array5 = {1,2,3,4,5};		
+		int[] array6 = {6,6,6,6,6,5,3,2,4,1,2,5,5,5};		
+
+		insertionSort(array1, 0, array1.length-1);
+		insertionSort(array2, 0, array2.length-1);
+		insertionSort(array3, 0, array3.length-1);
+		insertionSort(array4, 0, array4.length-1);
+		insertionSort(array5, 0, array5.length-1);
+		insertionSort(array6, 5, 9);
+
+		
+		System.out.println(Arrays.toString(array1));
+		System.out.println(Arrays.toString(array2));
+		System.out.println(Arrays.toString(array3));
+		System.out.println(Arrays.toString(array4));
+		System.out.println(Arrays.toString(array5));
+		System.out.println(Arrays.toString(array6));
+
 	}
 }
